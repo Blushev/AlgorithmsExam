cc84376fb00799fbbb5a98ed3d4c136c23a47f2d
Blushev <egor.rumyancev.9494@mail.ru>
2023-01-01 15:31:31 +0500

    Add Stack

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
-﻿internal class Program
+﻿using AlgorithmsExam.Stack;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        MyStack<string> stack = new MyStack<string>(4);
+        stack.Push("Kate");
+        stack.Push("Sam");
+        stack.Push("Alice");
+        stack.Push("Tom");
+
+
+        stack.ShowStack(stack);
     }
 }
